84ec7583b188704a5d40bbe06acb11ac1e04db64
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-09-13 14:19:54 +0900

    Add board resources, this commit makes first bootable buildroot linux

---
+++ board/zynqmp/kria/kr260/post-image.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -eu
+
+TARGETDIR=$1
+FWUP_CONFIG=$2
+echo "Target directory is $TARGETDIR"
+
+BOARD_NAME="kr260"
+
+FWUP=$HOST_DIR/usr/bin/fwup
+
+FW_PATH=$BINARIES_DIR/$BOARD_NAME.fw
+IMG_PATH=$BINARIES_DIR/$BOARD_NAME.img
+
+# Build the firmware image (.fw file)
+echo "Creating firmware file..."
+PROJECT_ROOT=$(pwd) "$FWUP" -c -f "$FWUP_CONFIG" -o "$FW_PATH"
+
+# Build a raw image that can be directly written to
+# an SDCard (remove an exiting file so that the file that
+# is written is of minimum size. Otherwise, fwup just modifies
+# the file. It will work, but may be larger than necessary.)
+echo "Creating raw SDCard image file..."
+rm -f "$IMG_PATH"
+"$FWUP" -a -d "$IMG_PATH" -i "$FW_PATH" -t complete
+
